7e0fa770697442708c6b5df12e7b1e7403d2cc8a
Khaled Ahmed <khaled.ahmed@elwatannews.com>
2025-03-26 20:10:56 +0200

    home category

--- src/Components/Home/HomeCategory.js
+++ src/Components/Home/HomeCategory.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Spinner } from "react-bootstrap";
 import SubTiltle from "../Uitily/SubTiltle";
 import CategoryCard from "./../Category/CategoryCard";
 import clothe from "../../images/clothe.png";
@@ -23,25 +23,37 @@ const HomeCategory = () => {
   const category = useSelector((state) => state.allCategory.category);
   const loading = useSelector((state) => state.allCategory.loading);
 
-  console.log(category.data);
-  //   console.log(loading);
+  console.log(loading);
+
+  const colors = [
+    "#FFD3E8",
+    "#F4DBA5",
+    "#55CFDF",
+    "#0034FF",
+    "#FFD3E8",
+    "#0034F1",
+  ];
 
   return (
     <Container>
       <SubTiltle title="التصنيفات" btntitle="المزيد" pathText="/allcategory" />
       <Row className="my-2 d-flex justify-content-between">
-        {category.data ? (
-          category.data.map((item) => {
-            return (
-              <CategoryCard
-                title={item.name}
-                img={item.image}
-                background="#F4DBA4"
-              />
-            );
-          })
+        {loading === false ? (
+          category.data ? (
+            category.data.slice(0, 6).map((item, index) => {
+              return (
+                <CategoryCard
+                  title={item.name}
+                  img={item.image}
+                  background={colors[index]}
+                />
+              );
+            })
+          ) : (
+            <h4>لا يوجد تصنيفات</h4>
+          )
         ) : (
-          <h4>لا يوجد تصنيفات</h4>
+          <Spinner animation="border" variant="primary" />
         )}
       </Row>
     </Container>
